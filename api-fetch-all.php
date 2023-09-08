866f691457008d9cda86c4928737e88b0f5bf3a6
Shahjalal <rjshahjalal132@gmail.com>
2023-09-08 10:57:56 +0600

    some modified

--- api-fetch-all.php
+++ api-fetch-all.php
@@ -1,5 +1,6 @@
 <?php
 
+// Headers
 header( 'Content-Type: application/json' );
 header( 'Access-Control-Allow-Origin: *' );
 
@@ -8,6 +9,7 @@ include "config.php";
 $sql    = "SELECT * FROM students";
 $result = mysqli_query( $conn, $sql ) or die( "Query Failed" );
 
+// Condition check if rows exits more then 0
 if ( mysqli_num_rows( $result ) > 0 ) {
 
     $output = mysqli_fetch_all( $result, MYSQLI_ASSOC );
